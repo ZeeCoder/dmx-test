245b5a13953a191832915d37518ffb40d57eb734
Viktor Hubert <rpgmorpheus@gmail.com>
2020-01-03 09:56:43 +0100

    tweaks

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const DMX = require("dmx");
 
 // --- Config ---
 // @see https://github.com/node-dmx/dmx#dmxregisterdrivername-module
-const module = "enttec-usb-dmx-pro";
+const driverName = "enttec-usb-dmx-pro";
 // @see https://github.com/node-dmx/dmx#dmxadduniversename-driver-device_id-options
-const deviceId = "";
+const deviceId = "your device id here";
 const deviceOptions = {};
 // @see https://github.com/node-dmx/dmx#dmxupdateuniverse-channels
 const updateToChannels = {};
@@ -12,7 +12,6 @@ const updateToChannels = {};
 
 const dmx = new DMX();
 
-dmx.registerDriver("enttec-driver", module);
-dmx.addUniverse("default-universe", "enttec-driver", deviceId, deviceOptions);
+dmx.addUniverse("default-universe", driverName, deviceId, deviceOptions);
 
 dmx.update("default-universe", updateToChannels);
